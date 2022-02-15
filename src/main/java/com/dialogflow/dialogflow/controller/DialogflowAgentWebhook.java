88a813b293d2204164352fd0430cff277fdcb805
Kobewen248 <99469520+Kobewen248@users.noreply.github.com>
2022-02-15 13:12:21 +0800

    Update DialogflowAgentWebhook.java

--- src/main/java/com/dialogflow/dialogflow/controller/DialogflowAgentWebhook.java
+++ src/main/java/com/dialogflow/dialogflow/controller/DialogflowAgentWebhook.java
@@ -1,19 +1,14 @@
 package com.dialogflow.dialogflow.controller;
 
+import com.dialogflow.dialogflow.entity.PageInfo;
 import com.dialogflow.dialogflow.entity.WebhookRequest;
 import com.dialogflow.dialogflow.entity.WebhookResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import java.io.BufferedWriter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
@@ -25,11 +20,15 @@ public class DialogflowAgentWebhook{
 	public WebhookResponse service(@RequestBody WebhookRequest webHookRequest) throws Exception {
 		System.out.println("request: " + new ObjectMapper().writeValueAsString(webHookRequest));
 		WebhookResponse webhookResponse = new WebhookResponse();
-		//webHookRequest.getSessionInfo().getParameters().put("isGuest", "true");
+		webHookRequest.getSessionInfo().getParameters().put("isGuest", "true");
 		webHookRequest.getSessionInfo().getParameters().put("IntentName",null);
-		webHookRequest.getSessionInfo().getParameters().put("usecase_id",null);
-		webHookRequest.getSessionInfo().getParameters().put("usecase_id",null);
+		webHookRequest.getSessionInfo().getParameters().put("usecase_id","bot");
 		webhookResponse.setSessionInfo(webHookRequest.getSessionInfo());
+		String currentPage = webHookRequest.getPageInfo().getCurrentPage();
+		int index = currentPage.lastIndexOf("/");
+		String str = currentPage.substring(index+1);
+		String endSession = currentPage.replace(str, "endSession");
+		webhookResponse.setTargetPage(endSession);
 		System.out.println("response: " + new ObjectMapper().writeValueAsString(webhookResponse));
 		return webhookResponse;
 		
